4f48091f59ba46f8bb400cbf2d1561a32d97e9a8
Andrii Matveev <matveev.tesla@gmail.com>
2022-12-30 16:25:55 +0200

    v1.01 has been implemented

---
+++ src/main/java/cinema/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package cinema.security;
+
+import cinema.model.User;
+import cinema.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userService.findByEmail(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found by email" + username));
+        org.springframework.security.core.userdetails.User.UserBuilder builder =
+                org.springframework.security.core.userdetails.User.withUsername(username);
+        builder.password(user.getPassword());
+        builder.roles(user.getRoles().stream()
+                .map(r -> r.getName().name())
+                .toArray(String[]::new));
+        return builder.build();
+    }
+}
